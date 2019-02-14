ac4a997879b3a7fd1beabc2cdf70ddbae632244e
djeeno <djeeeno@gmail.com>
2019-02-14 11:57:33 +0900

    vultr: fix test

--- .tools/test.sh
+++ .tools/test.sh
@@ -4,13 +4,13 @@ set -e -o pipefail
 
 DIRECTORY="$(echo "$(pwd)/$(git rev-parse --show-cdup)" | sed 's@/$@@')"
 BIN_NAME=vultr
-LATEST=$(git ls-remote --refs --tags https://github.com/djeeno/${BIN_NAME}-cli.git | grep -Eo "[0-9\.]*$" | sort -V | tail -1)
+LATEST=$(git ls-remote --refs --tags https://github.com/djeeno/${BIN_NAME}-cli.git | grep -Eo "[0-9\.]*$" | sort -V | tail -1) || true
 
 ##
 # README.md
 ##
 printf "\e[1;37m%s\e[0m\n" "$(LANG=C date) [TEST]  -- git diff README.md Testing ----------------"
-perl -pe 's@(curl -LRsS https://raw.githubusercontent.com/djeeno/'"${BIN_NAME}"'-cli)/[^/]+/('"${BIN_NAME}"' -o /tmp/'"${BIN_NAME}"')@\1/'"${LATEST}"'/\2@' -i "${DIRECTORY}/README.md"
+perl -pe 's@(curl -LRsS https://raw.githubusercontent.com/djeeno/'"${BIN_NAME}"'-cli)/[^/]+/('"${BIN_NAME}"' -o /tmp/'"${BIN_NAME}"')@\1/'"${LATEST:="0.0.1"}"'/\2@' -i "${DIRECTORY}/README.md"
 if [ -z "$(git diff ./README.md)" ]; then
   printf "\e[1;32m%s\e[0m\n" "$(LANG=C date) [INFO]  -- git diff README.md Passed ----------------"
 else
